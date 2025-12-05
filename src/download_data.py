692da3cdc672a6d530f21a7f1c14c6d1f6e8b2ce
Vienne Lee <vlee98@student.ubc.ca>
2025-12-05 13:20:57 -0800

    updated script

--- src/download_data.py
+++ src/download_data.py
@@ -4,14 +4,11 @@ import os
 
 @click.command()
 @click.option('--url', type=str, help="URL of dataset to be downloaded")
-#@click.option('--write_to', 
-                    #type=str, 
-                    #help="Path to directory where raw data will be written to",
-                    #default = "./data/iris.csv")
+
 def main(url):
     """Downloads data csv data from the web to a local filepath and extracts it."""
+    data = pd.read_csv(url)
     try:
-        data = pd.read_csv(url)
         data.to_csv('../data/raw/iris.csv')
     except:
         os.makedirs('../data/raw/')
