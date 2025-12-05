4da23dfef077eb7683eba296f54985c48ed47a00
Vienne Lee <vlee98@student.ubc.ca>
2025-12-05 14:04:30 -0800

    replaced try/except lines

--- src/split_preprocess.py
+++ src/split_preprocess.py
@@ -9,19 +9,19 @@ import os
 @click.option('--rawdata', type=str, help="Path to raw data", flag_value='../data/raw/iris.csv')
 def main(rawdata):
     """Perform data validation on raw data, clean up the data column names, and split the data into train and test data set"""
+
     df = pd.read_csv(rawdata)
     df = df.loc[:, 'sepal_length':]
-    data_validation = validate_data(df)
+    validated_data = validate_data(df)
 
-    train_df, test_df = train_test_split(data_validation, test_size=0.3, random_state=123)
+    train_df, test_df = train_test_split(validated_data, test_size=0.3, random_state=123)
 
-    try:
-        train_df.to_csv('../data/processed/iris_train.csv')
-        test_df.to_csv('../data/processed/iris_test.csv')
-    except:
-        os.makedirs('../data/processed/')
-        train_df.to_csv('../data/processed/iris_train.csv')
-        test_df.to_csv('../data/processed/iris_test.csv')
+    output_dir = "../data/processed/"
+    os.makedirs(output_dir, exist_ok=True)
+
+    train_df.to_csv(os.path.join(output_dir, "iris_train.csv"), index=False)
+    test_df.to_csv(os.path.join(output_dir, "iris_test.csv"), index=False)
+    print('The train and test data are saved in ../data/processed folder')
     
 if __name__ == '__main__':
     main()
