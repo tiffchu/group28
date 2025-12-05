7c9e55343ae9b4a5bbe89167206a9859ca7ec39f
Vienne Lee <vlee98@student.ubc.ca>
2025-12-05 13:27:18 -0800

    added default value

--- src/download_data.py
+++ src/download_data.py
@@ -3,16 +3,18 @@ import pandas as pd
 import os
 
 @click.command()
-@click.option('--url', type=str, help="URL of dataset to be downloaded")
+@click.option('--url', type=str, help="URL of dataset to be downloaded", flag_value= 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
 
 def main(url):
     """Downloads data csv data from the web to a local filepath and extracts it."""
     data = pd.read_csv(url)
     try:
         data.to_csv('../data/raw/iris.csv')
-    except:
+    except Exception as e:
         os.makedirs('../data/raw/')
         data.to_csv('../data/raw/iris.csv')
+        print("Error:", e)
+        
 
 if __name__ == '__main__':
     main()
