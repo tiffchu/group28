26c75c1e9485fc73afb94beabd1292a6e104f966
Vienne Lee <vlee98@student.ubc.ca>
2025-12-05 14:23:59 -0800

    added some lines

--- src/download_data.py
+++ src/download_data.py
@@ -3,19 +3,26 @@ import pandas as pd
 import os
 
 @click.command()
-@click.option('--url', type=str, help="URL of dataset to be downloaded", 
-default= "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv", show_default=True)
+@click.option('--url', type=str, help="URL of dataset to be downloaded", default= "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv", show_default=True)
 
 def main(url):
     """Downloads data csv data from the web to a local filepath and extracts it."""
+
     data = pd.read_csv(url)
+
+    output_path = "../data/raw/iris.csv"
+
+    #ensure path exists
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
     try:
         data.to_csv('../data/raw/iris.csv')
         print('The Iris data is saved in ../data/raw directory.')
+
     except Exception as e:
         os.makedirs('../data/raw/')
         data.to_csv('../data/raw/iris.csv')
-        print("Error:", e)
+        print("Failed to download or save data:", e)
         print('The Iris data is saved in ../data/raw directory.')
         
 
