c7e66902f81114d54eb376df4bccae3f20c34f65
Nguyen Nguyen <nguyenuyennguyen03@gmail.com>
2025-12-11 13:43:00 -0800

    add docstring

--- src/save_model.py
+++ src/save_model.py
@@ -1,15 +1,32 @@
-# src/save_helpers.py
+# src/save_model.py
 import pickle
 import os
 
 def save_model(model, path):
     """
-    Docstring for save_model
+    This function saves the best model after optimizing its hyperparameters during training using pickle.
+
+    This function serves as a reusable step for saving models so they can be easily 
+    loaded and reused in the future. It also helps reduce the amount of repeated code. 
     
-    :param model: Description
-    :param path: Description
-    
+    Parameters
+    ----------
+    model : any
+        The optimized trained model. After model.best_estimator_
+    path : str
+        The file path where the model is saved to.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> model = decision_tree_random_search.best_estimator_
+    >>> save_model(model, f"{models_to}/decision_tree.pickle")
+
     """
+
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'wb') as f:
         pickle.dump(model, f)
