526acbff80eeaa5759dfce9de7925c000b2f3627
tiffchu <tiffanymchu@gmail.com>
2025-12-13 11:18:59 -0800

    fixed spelling in eda

--- src/make_pairwise.py
+++ src/make_pairwise.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import altair as alt
 
-def make_pairwise_plot(train_data, color, column, row):
+def make_pairwise_plot(train_data, color, columns):
     pairwise_plot = alt.Chart(train_data).mark_point(opacity=0.3, size=10).encode(
-        alt.X(alt.repeat('row'), type='quantitative'),
-        alt.Y(alt.repeat('column'), type='quantitative'),
+        alt.X(alt.repeat('column'), type='quantitative'),
+        alt.Y(alt.repeat('row'), type='quantitative'),
         color = alt.Color(
         color, type="nominal",
+        title = 'species',
         scale=alt.Scale(
             domain=['setosa', 'versicolor', 'virginica'],
             range=['blue', 'orange', 'green'])
-        ).title("Species")
-            ).properties(
+        )).properties(
                 width=150,
                 height=150
             ).repeat(
-                column=column,
-                row=row
-            )
+        row=columns,
+        column=columns)
+    
     return pairwise_plot
 
 
